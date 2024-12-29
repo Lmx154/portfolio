7e915113b813008fc8e367e0e41ee4c34ef4e0c2
Luis <luis_mtz907@live.com>
2024-12-29 12:56:11 -0600

    recentered welcome message

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,22 +9,24 @@ const Home = () => {
         alt="Portrait"
         className="w-48 h-48 rounded-full object-cover drop-shadow-lg fading-mask mb-8"
       />
-      <div className="h-[160px] flex items-center justify-center">
-        <h1 className="text-6xl font-bold gradient-text pb-4 leading-normal">
-          <Typewriter
-            words={[
-              'Welcome to My Portfolio.', 
-              'I\'m Luis, a passionate Software Developer.',
-              'Let\'s Build Something Together.'
-            ]}
-            loop={1}
-            cursor
-            cursorStyle='█'
-            typeSpeed={80}
-            deleteSpeed={50}
-            delaySpeed={2000} // Increased delay between messages
-          />
-        </h1>
+      <div className="relative h-[200px] w-full">
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full px-4">
+          <h1 className="text-3xl md:text-4xl lg:text-6xl font-bold gradient-text pb-4 leading-normal text-center mx-auto">
+            <Typewriter
+              words={[
+                'Welcome to My Portfolio.', 
+                'I\'m Luis, a passionate Software Developer.',
+                'Let\'s Build Something Together.'
+              ]}
+              loop={1}
+              cursor
+              cursorStyle='█'
+              typeSpeed={80}
+              deleteSpeed={50}
+              delaySpeed={2000} // Increased delay between messages
+            />
+          </h1>
+        </div>
       </div>
       <div className="arrow-container">
         <svg className="arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
