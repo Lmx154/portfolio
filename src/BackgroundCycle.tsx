361e9b15d92109a6c34bbfd0e0458d3253643eda
Luis <luis_mtz907@live.com>
2024-12-29 22:34:03 -0600

    fixed background refactored to work on apple devices

--- src/BackgroundCycle.tsx
+++ src/BackgroundCycle.tsx
@@ -24,9 +24,8 @@ export default function BackgroundCycle() {
     }
   }, [])
 
-
   return (
-    <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }}>
+    <div className="background-container">
       {backgrounds.map((bg, index) => (
         <div
           key={index}
